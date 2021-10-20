65a1e370382581c43b5dc83258dec9149ceda533
Martin <googldu59@gmail.com>
2021-10-20 08:13:20 +0200

    wip2

--- script/SoundView.js
+++ script/SoundView.js
@@ -17,22 +17,22 @@ export class SoundView extends NodePose3D { // intègre this.node placé par la 
         for (let k = 0; k < nBar; k++) {
             let cube = new THREE.Mesh(geometry, material);
             this.node.add(cube);
-            cube.position.copy(new THREE.Vector3(0 + k, 0, -2));
+            cube.position.copy(new THREE.Vector3(k, 0, -2));
         }
     }
 
     update() {
         super.update(); // assure la position de la vue selon la pose du marqueur associé
         // copie les données de l'analyse
-        this.tool.equalizer; // aller chercher le tabl
+        // aller chercher le tabl
         // exploiter this.tool.dataArray pour la mise à jour de la vue (redimensionner chaque barre)
         // (dataArray provient de la décomposition en fréquence du son : chaque valeur du tableau est comprise entre 0 et 255 représentant l'amplitude : voir ci-après).
-        if(this.tool.dataArray !== undefined){
-            for (let k = 0; k < 8; k++) {
-                let values = this.dataArray[k * 2] + this.dataArray[k * 2 + 1]; // [0 -510]
-                let transformedValue = (values / 100) + 0.5
-                this.node[k].scale.set(transformedValue, 1, 1);
-            }
+        let dataArray = this.tool.dataArray;
+        for (let k = 0; k < 8; k++) {
+            let values = dataArray[k * 2] + dataArray[k * 2 + 1]; // [0 -510]
+            let transformedValue = (values / 100) + 0.5
+            this.node.children[k].scale.set(transformedValue, 1, 1);
         }
+
     }
 }
